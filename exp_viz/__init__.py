a2d0d654a46bb7a5c2bdcd5613476d19a520dbc5
Bianca Cunha <mcunhabianca@gmail.com>
2023-11-13 21:45:47 -0300

    atuailzando readme

--- exp_viz/__init__.py
+++
@@ -1,3 +0,0 @@
-from .exp_explainers import SHAPViz
-from .exp_explainers import LIMEViz
-from .exp_explainers import Explainer
--- exp_viz/exp_explainers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .shap_viz import SHAPViz
-from .lime_viz import LIMEViz
-from .explainer import Explainer
